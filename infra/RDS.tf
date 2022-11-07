697c522083699fd271aa42c5d7786a9e89703cf9
Leonardo de Macedo Sartorello <45469728+leollo98@users.noreply.github.com>
2022-11-06 22:57:41 -0300

    Create RDS.tf

---
+++ infra/RDS.tf
@@ -0,0 +1,9 @@
+resource "aws_db_instance" "default" {
+  allocated_storage    = 10
+  db_name              = "root"
+  engine               = "postgres"
+  instance_class       = "db.t3.micro"
+  username             = "postgres"
+  password             = "root"
+  skip_final_snapshot  = true
+}
