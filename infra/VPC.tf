7a124ff94bd54278d92fc7ab7e07d82700916e47
Leonardo de Macedo Sartorello <45469728+leollo98@users.noreply.github.com>
2022-11-08 14:34:06 -0300

    Update VPC.tf

--- infra/VPC.tf
+++ infra/VPC.tf
@@ -7,6 +7,7 @@ module "vpc" {
   azs             = ["us-west-2a", "us-west-2b", "us-west-2c"]
   private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
   public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+  database_subnets= ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
   
   create_database_subnet_group           = true
   create_database_subnet_route_table     = true
