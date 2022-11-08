9467109d5f40b7edb539c47bdcb2c8dd34f44f9d
Leonardo de Macedo Sartorello <45469728+leollo98@users.noreply.github.com>
2022-11-08 14:25:26 -0300

    Update RDS.tf

--- infra/RDS.tf
+++ infra/RDS.tf
@@ -7,6 +7,6 @@ resource "aws_db_instance" "default" {
   password             = "rootroot"
   skip_final_snapshot  = true
   publicly_accessible  = true
-  
+  db_subnet_group_name = module.vpc.database_subnet_group_name
   vpc_security_group_ids = [aws_security_group.alb.id]
 }
