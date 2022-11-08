3f6f0ed566c8ccd3681f7ec3d0fdff3cd7a0a57c
Leonardo de Macedo Sartorello <45469728+leollo98@users.noreply.github.com>
2022-11-08 15:11:03 -0300

    Update GrupoSeguranca.tf

--- infra/GrupoSeguranca.tf
+++ infra/GrupoSeguranca.tf
@@ -11,7 +11,22 @@ resource "aws_security_group_rule" "tcp_alb" {
   cidr_blocks       = ["0.0.0.0/0"] #0.0.0.0 - 255.255.255.255
   security_group_id = aws_security_group.alb.id
 }
-  resource "aws_security_group_rule" "tcp_alb" {
+
+resource "aws_security_group_rule" "saida_alb" {
+  type              = "egress"
+  from_port         = 0
+  to_port           = 0
+  protocol          = "-1"
+  cidr_blocks       = ["0.0.0.0/0"] #0.0.0.0 - 255.255.255.255
+  security_group_id = aws_security_group.alb.id
+}
+  
+resource "aws_security_group" "db" {
+  name        = "alb_ECS"
+  vpc_id      = module.vpc.vpc_id
+}
+
+resource "aws_security_group_rule" "tcp_alb" {
   type              = "ingress"
   from_port         = 5432
   to_port           = 5432
