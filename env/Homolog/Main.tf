72e64a71f9e44fed7d10999680abad6716de78f7
Leonardo de Macedo Sartorello <45469728+leollo98@users.noreply.github.com>
2022-11-08 15:03:38 -0300

    Update Main.tf

--- env/Homolog/Main.tf
+++ env/Homolog/Main.tf
@@ -4,7 +4,7 @@ module "homolog" {
     nome_repositorio = "homolog"
     cargoIAM = "homolog"
     ambiante = "homolog"
-    imagem="leonardosartorello/go_ci:100"
+    imagem="leonardosartorello/go_ci:22"
 }
 
 output "IP_alb" {
